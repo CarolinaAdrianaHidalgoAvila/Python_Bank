f2220b47b2eeffc934f6939ec9af0f81f5e449a1
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-10-09 00:12:54 -0400

    Added basic test of BankAccount

--- Tests/BankAccount_test.py
+++ Tests/BankAccount_test.py
@@ -1,5 +1,16 @@
 import pytest
+from Code.BankAccount.BankAccount import BankAccount
 
-# simple test
-def test_function():
-    assert True == True
+
+def test_deposit():
+    # Arrange
+    one_bank_account = BankAccount()
+    # Act
+    income = one_bank_account.deposit(100)
+    # Assert
+    assert income == 100
+
+def test_withdraw():
+    one_bank_account = BankAccount()
+    income = one_bank_account.withdraw(50)
+    assert income == -50
