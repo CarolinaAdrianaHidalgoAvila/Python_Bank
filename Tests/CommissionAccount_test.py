57c47db4faf3fef833c50fc47258958d7ce95b5c
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-10-09 12:28:34 -0400

    Fixed  call of modules

---
+++ Tests/CommissionAccount_test.py
@@ -0,0 +1,7 @@
+import pytest
+from Code.BankAccount.CommissionAccount import CommissionAccount
+
+#def test_getMonthlyReport():
+#    one_bank_account = CommissionAccount(100000087454, 124857)
+#    income = one_bank_account.getMonthlyReport( 0.02)
+#    assert income == -50
