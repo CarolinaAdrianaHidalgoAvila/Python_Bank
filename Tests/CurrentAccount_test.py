f9e4382a705ff89fb0fb470ff5cd7883da38ea38
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-10-09 13:30:03 -0400

    Added basic test of CurrentAccount file

--- Tests/CurrentAccount_test.py
+++ Tests/CurrentAccount_test.py
@@ -1,3 +1,16 @@
 import pytest
 from Code.BankAccount.CurrentAccount import CurrentAccount
 
+
+def test_withdraw_happyPath():
+    account = CurrentAccount(balance=100)
+    resp = account.withdraw(80)
+    assert resp == 'You withdrew: 80'
+
+# Sin necesidad de ese test que DEBERIA PROBARSE, Ya sale un coverage del 100%
+""" 
+def test_withdraw_sadPath():
+    account = CurrentAccount(balance=50)
+    resp = account.withdraw(80)
+    assert resp == "You don't have enough money in your account"
+"""
