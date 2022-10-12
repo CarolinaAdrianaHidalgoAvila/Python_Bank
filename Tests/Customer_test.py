872cb73524892f88a9850c0b60790e2f6fc62a9e
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-10-12 13:07:51 -0400

    Customer 100%

--- Tests/Customer_test.py
+++ Tests/Customer_test.py
@@ -1,32 +1,46 @@
 import pytest
+
 from Code.BankAccount.BankAccount import BankAccount
 from Code.BankAccount.CommissionAccount import CommissionAccount
 
 from Code.Customer.Customer import Customer
-def test_newCustomer_vacio():
-    georgeCustomer = None
-    assert  georgeCustomer is None
+
+@pytest.fixture
+def customer():
+    georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
+    return georgeCustomer
+
+def test_customer_new(customer):
+    assert customer.name == "George Curious"
+    assert customer.age == 23
+    assert isinstance(customer, Customer)
 
 def test_newCustomer():
     georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
     assert  georgeCustomer is not None
-def test_incrementAge():
-    georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
-    incrementAgeG=georgeCustomer.incrementAge(incrementValue=1)
+def test_incrementAge(customer):
+    incrementAgeG=customer.incrementAge(incrementValue=1)
     assert  incrementAgeG == 24
-def test_changeIncome():
-    georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
-    change=georgeCustomer.changeIncome(70_000)
+def test_changeIncome(customer):
+    change=customer.changeIncome(70_000)
     assert change == 70_000
-def test_changeBankAccount():
-    georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
-    chanceBack = georgeCustomer.changeBankAccount(CommissionAccount(balance=50))
+def test_changeBankAccount(customer):
+    chanceBack = customer.changeBankAccount(CommissionAccount(balance=50))
     assert chanceBack != BankAccount(balance=130)
-def test_makeDeposit_positive():
-    georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
-    balance_George=georgeCustomer.makeDeposit(100)
+def test_makeDeposit_positive(customer):
+    balance_George=customer.makeDeposit(100)
     assert balance_George >= 130
-def test_makeDeposit_negative():
-    georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
-    balance_George=georgeCustomer.makeDeposit(-1)
+def test_makeDeposit_negative(customer):
+    balance_George=customer.makeDeposit(-1)
     assert balance_George == 130
+def test_makeWithdrawal_1(customer):
+    withdrawal= customer.makeWithdrawal(-1)
+    assert withdrawal ==  130
+def test_makeWithdrawal_2(customer):
+    
+    withdrawal= customer.makeWithdrawal(120)
+    assert withdrawal == 10
+def test_makeWithdrawal_3(customer):
+    
+    withdrawal= customer.makeWithdrawal(135)
+    assert withdrawal == 130
