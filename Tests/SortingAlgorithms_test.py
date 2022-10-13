80666625bd41508d43c4c4b2fd1ca16fe542b736
LuisSebastian28 <73673100+LuisSebastian28@users.noreply.github.com>
2022-10-13 11:34:15 -0400

    quicksort_v2

--- Tests/SortingAlgorithms_test.py
+++ Tests/SortingAlgorithms_test.py
@@ -1,18 +1,47 @@
 import pytest
 from Code.SortingAlgorithms.QuickSort import quicksort
+from Code.SortingAlgorithms.BubbleSort import bubbleSort
 
-def test_changePreferredInvestment():
-    costumerStart = quicksort(array=[])
-    assert costumerStart==[]
 
-def test_changePreferredInvestment():
-    costumerStart = quicksort(array=[1])
-    assert costumerStart==[1]
 
-def test_changePreferredInvestment():
-    costumerStart = quicksort(array=[1,2])
-    assert costumerStart==[1,2]
+def test_quicksort():
+    resp = quicksort(array=[])
+    assert resp==[]
 
-def test_changePreferredInvestment():
-    costumerStart = quicksort(array=[1,2,3])
-    assert not costumerStart==[1,2,3]
+def test_quicksort_2():
+    resp = quicksort(array=[1])
+    assert resp==[1]
+
+def test_quicksort_3():
+    resp = quicksort(array=[1,2])
+    assert resp==[1,2]
+
+def test_quicksort_4():
+    resp = quicksort(array=[1,2,3])
+    assert  resp==[1,2,3]
+
+def test_quicksort_4():
+    resp = quicksort(array=[2,1,3])
+    assert  resp==[1,2,3]
+
+#bubblesort
+
+def test_bubbleSort():
+    resp = bubbleSort(theSeq=[])
+    assert resp==[]
+
+def test_bubbleSort():
+    resp = bubbleSort(theSeq=[1])
+    assert resp==[1]
+
+def test_bubbleSort():
+    resp = bubbleSort(theSeq=[1,2])
+    assert resp==[1,2]
+
+def test_bubbleSort():
+    resp = bubbleSort(theSeq=[1,2,3])
+    assert resp==[1,2,3]
+
+def test_bubbleSort():
+    resp = bubbleSort(theSeq=[2,1,3])
+    assert resp==[1,2,3]
