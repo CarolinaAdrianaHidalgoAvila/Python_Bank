5e17ae11dff2d2f019435611c3e149c3fee299ff
CarolinaMelendez <venecia.melendez@ucb.edu.bo>
2022-10-13 13:30:04 -0400

    Added covergerc and fixed coverage of sorting test

--- Tests/SortingAlgorithms_test.py
+++ Tests/SortingAlgorithms_test.py
@@ -16,32 +16,32 @@ def test_quicksort_3():
     resp = quicksort(array=[1,2])
     assert resp==[1,2]
 
-def test_quicksort_4():
+def test_quicksort_4_a():
     resp = quicksort(array=[1,2,3])
     assert  resp==[1,2,3]
 
-def test_quicksort_4():
+def test_quicksort_4_b():
     resp = quicksort(array=[2,1,3])
     assert  resp==[1,2,3]
 
 #bubblesort
 
-def test_bubbleSort():
+def test_bubbleSort_a():
     resp = bubbleSort(theSeq=[])
     assert resp==[]
 
-def test_bubbleSort():
+def test_bubbleSort_b():
     resp = bubbleSort(theSeq=[1])
     assert resp==[1]
 
-def test_bubbleSort():
+def test_bubbleSort_c():
     resp = bubbleSort(theSeq=[1,2])
     assert resp==[1,2]
 
-def test_bubbleSort():
+def test_bubbleSort_d():
     resp = bubbleSort(theSeq=[1,2,3])
     assert resp==[1,2,3]
 
-def test_bubbleSort():
+def test_bubbleSort_e():
     resp = bubbleSort(theSeq=[2,1,3])
     assert resp==[1,2,3]
