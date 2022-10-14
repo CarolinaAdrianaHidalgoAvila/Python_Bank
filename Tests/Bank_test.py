20fd142e7d8da06aa19d7c28b09e13b0b19f1ce3
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-10-13 22:42:46 -0400

    test bank 97%

--- Tests/Bank_test.py
+++ Tests/Bank_test.py
@@ -27,8 +27,34 @@ def test_addCustomer(bank):
     assert len(bank.customers)>4
     assert res == True
 def test_addCustomer_fail(bank):
-    georgeCustomer = Customer('George Curious', 23, 60_000, BankAccount(balance=130))
     res = bank.addCustomer(bank.customers[2])
     assert len(bank.customers) == 4
     assert res == False
-def 
+def test_removeCustomer_path_4(bank):
+    resp = bank.removeCustomer(customer=bank.customers[2], customerName=None)
+    assert resp == True
+
+def test_removeCustomer_path_5(bank):
+    reneCustomer = Customer('Rene Curious', 23, 60_000, BankAccount(balance=130))
+    bank.addCustomer(reneCustomer)   
+    resp = bank.removeCustomer(customer=None, customerName=bank.customers[2].name)
+    assert resp == True
+
+def test_removeCustomer_path_6(bank):
+    reneCustomer = Customer('Rene Curious', 23, 60_000, BankAccount(balance=130))
+    bank.addCustomer(reneCustomer)
+    resp = bank.removeCustomer(customer=None, customerName=bank.customers[4].name) 
+    assert resp == True
+
+
+def test_removeCustomer_path_7(bank,): 
+    resp = bank.removeCustomer(customer=None, customerName=bank.customers[2].name)
+    assert resp == True
+
+def test_removeCustomer_path_8(bank):
+    resp = bank.removeCustomer(customer=None, customerName="Jorge")
+    assert resp == False
+
+def test_removeCustomer_path_9(bank):
+    resp = bank.removeCustomer(customer=None, customerName=None)
+    assert resp == False
